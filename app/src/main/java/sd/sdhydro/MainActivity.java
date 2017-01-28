51ab3a7739588339977661ada07d78e03dd68476
Alex Costello <alcostello91@gmail.com>
2017-01-28 14:36:29 -0500

    Initial commit

---
+++ app/src/main/java/sd/sdhydro/MainActivity.java
@@ -0,0 +1,54 @@
+package sd.sdhydro;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.let);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        //clicked on settings in menu
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        //clicked on about in menu
+        if (id == R.id.action_about) {
+            System.out.println("clicked about");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
